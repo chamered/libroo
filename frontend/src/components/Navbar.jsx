bb485a0ca5852685c8f41df2f7ac7ba7bbf5f341
chamered <smaltauro@unibz.it>
2025-05-30 13:14:28 +0200

    Added hamburger menu in navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -6,10 +6,13 @@ function Navbar() {
     const currentPath = location.pathname;
 
     return(
-        <nav className="navbar navbar-expand-lg bg-body-tertiary fixed-top">
+        <nav className="navbar navbar-expand-sm bg-body-tertiary fixed-top">
             <div className="container-fluid">
                 <Link className="navbar-brand fw-bold d-flex align-items-center" to="/"><SquareLibrary className="text-primary" size={32}/>Libroo</Link>
-                <div className="collapse navbar-collapse">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNav">
                     <div className="navbar-nav">
                         <Link className={`nav-link ${currentPath === "/" ? "active fw-medium" : ""}`} to="/">Home</Link>
                         <Link className={`nav-link ${currentPath === "/dashboard" ? "active fw-medium" : ""}`} to="/dashboard">Dashboard</Link>
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import './css/index.css'
 import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap.bundle.js'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
